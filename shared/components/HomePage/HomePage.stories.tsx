12a7dbfc5d57db3176723de847050e5bb2442375
Frank Cooke <frankcooke79@gmail.com>
2019-07-12 16:27:30 -0400

    various fixes

--- shared/components/HomePage/HomePage.stories.tsx
+++ shared/components/HomePage/HomePage.stories.tsx
@@ -1,17 +1,16 @@
+import React from 'react';
 import { storiesOf } from '@storybook/react';
-import * as React from 'react';
-import { HomePage } from './Component';
 import { withKnobs, text } from '@storybook/addon-knobs';
+import { HomePage } from './Component';
 
 storiesOf('HomePage', module)
-    .addDecorator(withKnobs)
-    .add('Default Page', () => 
-        (<HomePage mainText="Lorem Ipsum" cta={{text: 'Do Something'}} />)
-    )
-    .add('Dynamic Page', () => {
-        const mainText = text('Main Text', 'Lorem Ipsum');
-        const cta = text('ctaText', 'Do Something');
-        return (<HomePage mainText={mainText} cta={{text: cta}} />);
-    }
-    
+  .addDecorator(withKnobs)
+  .add('Default Page', () =>
+      (<HomePage title="Lorem Ipsum" cta={{text: 'Do Something'}} />)
+  )
+  .add('Dynamic Page', () => {
+      const title = text('Title', 'Lorem Ipsum');
+      const cta = text('CTA Text', 'Do Something');
+      return (<HomePage title={title} cta={{ text: cta }} />);
+  }
 );
--- shared/components/elements-ui/Button.stories.tsx
+++ shared/components/elements-ui/Button.stories.tsx
@@ -1,13 +1,15 @@
+import React from 'react';
 import { storiesOf } from '@storybook/react';
-import * as React from 'react';
-import Button from './Button';
 import { withKnobs, text } from '@storybook/addon-knobs';
+import { Button } from './Button';
 
 const stories = storiesOf('Button', module);
 stories.addDecorator(withKnobs);
+
 stories.add('with text', () => (
-    <Button label={text('Button Label', 'Hello World')} />
-  ));
+  <Button text={text('Button Label', 'Hello World')}/>
+));
+
 stories.add('with some emoji', () => (
-    <Button label="😀 😎 👍 💯" />
-  ));
+  <Button text="😀 😎 👍 💯"/>
+));
