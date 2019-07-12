65198d2828f846f12098f14cf4ae204807a3d6ee
Frank Cooke <frankcooke79@gmail.com>
2019-07-12 17:43:54 -0400

    adds simple styled-components theme

---
+++ shared/styled/theme.ts
@@ -0,0 +1,12 @@
+import { DefaultTheme } from 'styled-components';
+
+const styledTheme: DefaultTheme = {
+  borderRadius: '5px',
+
+  colors: {
+    main: 'cyan',
+    secondary: 'magenta',
+  },
+};
+
+export { styledTheme };
