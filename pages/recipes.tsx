f092441206ce176449c0d942518cef7935f4d49f
Frank Cooke <frankcooke79@gmail.com>
2019-07-12 17:48:03 -0400

    uses recipes page as example component for styled-components with ts

--- pages/recipes.tsx
+++ pages/recipes.tsx
@@ -1,28 +1,65 @@
 import React from 'react';
-import Link from 'next/link';
+import { Link } from '../shared/components/Link';
 import { useAllRecipes } from '../shared/hooks/recipe/useAllRecipes';
+import styled from 'styled-components';
+
+const RecipesWrapper = styled.div`
+  font-family: Tahoma, sans-serif; 
+  display: flex;
+  flex-direction: column;
+`;
+
+const TitleWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 20px 0;
+`;
+
+const PageTitle = styled.h1`
+  margin: 0 20px 0 0;
+  line-height: 42px;
+`;
+
+const List = styled.ul`
+  padding: 12px;
+  background: ${({ theme }) => theme.colors.secondary};
+`;
+
+const ListItem = styled.li`
+  padding: 20px;
+  list-style: none;
+  background: ${({ theme }) => theme.colors.main};
+  border-color: ${({ theme }) => theme.colors.secondary};
+  
+  &:not(:last-child) {
+    margin-bottom: 12px;
+  }
+  
+  h4 {
+    margin: 0 0 8px;
+  }
+`;
 
 const Recipes: React.FC<{}> = () => {
   const { recipes } = useAllRecipes();
 
   return (
-    <div>
-      <h1>Some Sweet Recipes</h1>
-      <ul>
-        {recipes.map(recipe => (
-          <li key={recipe.id}>
-            <div>
-              <h4 style={{ marginBottom: 5 }}>{recipe.title}</h4>
-              <span>{recipe.description}</span>
-            </div>
-          </li>
-        ))}
-      </ul>
+    <RecipesWrapper>
+      <TitleWrapper>
+        <PageTitle>Some Sweet Recipes</PageTitle>
 
-      <Link href="/">
-        <a>Go home</a>
-      </Link>
-    </div>
+        <Link href="/" size="large">Go home</Link>
+      </TitleWrapper>
+
+      <List>
+        {recipes.map(recipe => (
+          <ListItem key={recipe.id}>
+            <h4 style={{ marginBottom: 5 }}>{recipe.title}</h4>
+            <span>{recipe.description}</span>
+          </ListItem>
+        ))}
+      </List>
+    </RecipesWrapper>
   );
 };
 
