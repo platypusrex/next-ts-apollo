e61d82de93801c9ddc2ed5641911d1d54dc20574
Frank Cooke <frankcooke79@gmail.com>
2019-07-13 18:24:35 -0400

    updates storybook config and exposes styled-component theme to all stories

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,18 @@
-import { configure } from '@storybook/react';
+import React from 'react';
+import { configure, addDecorator } from '@storybook/react';
+import { styledTheme } from '../src/styled/theme.ts';
+import { ThemeProvider } from 'styled-components';
 
 // automatically import all files ending in *.stories.js
-const req = require.context('../shared/components', true, /\.stories.tsx$/);
+const req = require.context('../src/components', true, /\.stories.tsx$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
+
+addDecorator((story) => (
+  <ThemeProvider theme={styledTheme}>
+    {story(styledTheme)}
+  </ThemeProvider>
+));
 
 configure(loadStories, module);
